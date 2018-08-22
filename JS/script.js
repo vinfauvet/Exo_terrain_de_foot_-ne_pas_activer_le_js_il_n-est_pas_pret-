a4845ee78fa919cde39c49d24765e183b5f0490f
vincent <vinfauvet@gmail.com>
2018-08-22 14:56:39 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,49 @@
+// var h4 = document.getElementById('h4');
+// var ballon = document.getElementById('ballon');
+// var x_h4 = 0;
+// var y_h4 = 0;
+// var x_ballon = 50;
+// var x_ballon_block_droite = x_ballon + 2;
+// var y_ballon = 50;
+// var x_tape_y = y_ballon - 5;
+// ballon.style.left = x_ballon + "%";
+// ballon.style.top = y_ballon + "%";
+//
+// // setInterval(function () {
+// //   console.log("x_h4 = " + x_h4 + " x_ballon = " + x_ballon);
+// //   console.log("y_h4 = " + y_h4 + " y_ballon = " + y_ballon);
+// //   console.log("x_tape = " + x_tape);
+// // }, 3000);
+//
+// onkeypress = function (event) {
+//   if (event.keyCode == 37) {
+//
+//     if ( x_h4 !== x_ballon_block_droite && y_h4 !== y_ballon) {
+//
+//     x_h4 -= 5;
+//     h4.style.left = x_h4 + "%";
+//     h4.style.backgroundImage = "url('images/hg4.png')";
+//   }
+//   }else if (event.keyCode == 39) {
+//     x_h4 += 5;
+//     h4.style.left = x_h4 + "%";
+//     h4.style.backgroundImage = "url('images/h4.png')";
+//   }else if (event.keyCode == 38) {
+//     y_h4 -= 5;
+//     h4.style.top = y_h4 + "%";
+//     h4.style.backgroundImage = "url('images/h4h.png')";
+//   }else if (event.keyCode == 40) {
+//     y_h4 += 5;
+//     h4.style.top = y_h4 + "%";
+//     h4.style.backgroundImage = "url('images/h4b.png')";
+//   }
+//   // if (x_h4 == x_ballon && y_h4 == x_tape_y) {
+//   //   window.setTimeout(frappe_x, 400)
+//   //   function frappe_x() {
+//   //
+//   //   ballon.style.transform = "";
+//   //   x_ballon += 10;
+//   //   ballon.style.left = x_ballon + "%";
+//   // }
+//   //}
+// }
